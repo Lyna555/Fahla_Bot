2873c1a872fa34fa6e87012533b448ea3bf93d14
Lyna555 <bouglina3@gmail.com>
2025-03-26 09:27:56 +0100

    features

--- fahla_aud_bot.py
+++ fahla_aud_bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import uuid
 from telethon import TelegramClient, events
 from telethon.tl.types import ChannelParticipantsAdmins
 from pytgcalls import PyTgCalls
@@ -7,9 +8,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Telegram app information + Telegram account information
+# Telegram app information
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
+
+# Telegram userbot information
 PHONE_NUMBER = os.getenv("PHONE_NUMBER")
 CLIENT_CODE = os.getenv("CLIENT_CODE")
 
@@ -22,7 +25,7 @@ uploaded_files = {}
 SAVE_FOLDER = "./audios"
 os.makedirs(SAVE_FOLDER, exist_ok=True)
 
-bot_active = False
+active_groups = set()
 
 # verify if the command sender is an admin
 async def is_admin(event):
@@ -37,75 +40,73 @@ async def is_admin(event):
     
     return any(admin.id == sender_id for admin in admins)
 
-# start the bot
-@client.on(events.NewMessage(pattern="/ابدا"))
-async def start_bot(event):
-
-    global bot_active
-
-    # check if the sender if it's an admin
+# check if the sender if it's an admin
+async def check_if_admin(event):
     if not await is_admin(event):
         await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
         return
+    
+# check if the bot is active on this group
+async def check_if_bot_active(chat_id, event):
+    if chat_id not in active_groups:
+        await event.reply("⚠️ البوت غير مفعل في هذه المجموعة! استخدم `/ابدا` أولًا.")
+        return
 
-    bot_active = True
+# start the bot
+@client.on(events.NewMessage(pattern="/ابدا"))
+async def start_bot(event):
+    
+    check_if_admin(event)
+    
+    chat_id = event.chat_id
+    active_groups.add(chat_id)
 
     await event.reply("""✅ البوت مفعل الآن ويمكنك استخدام الأوامر!
 
     🔹 **كيف يعمل البوت؟**
     1️⃣ **تفعيل البوت**: عند إرسال `/ابدا`، يتم تشغيل البوت ويصبح جاهزًا للاستجابة للأوامر.
-    2️⃣ **حفظ الملفات الصوتية**: عند إرسال ملف `.mp3`، سيتم حفظه تلقائيًا (فقط من قبل المشرفين).
-    3️⃣ **تشغيل الصوت في المحادثة الصوتية**: قم بالرد على الملف الصوتي الذي أرسلنه واستخدم `/شغل` لتشغيله.
+    2️⃣ **حفظ الملفات الصوتية**: عند إرسال الملف الصوتي، سيتم حفظه تلقائيًا (فقط من قبل المشرفين).
+    3️⃣ **تشغيل الصوت في المحادثة الصوتية**: قم بالرد على الملف الصوتي الذي أرسلته وأرسل `/شغل` لتشغيله.
     4️⃣ **التحكم في التشغيل**:
     - ⏸ `/توقف` لإيقاف التشغيل مؤقتًا.
     - ▶ `/اكمل` لاستئناف التشغيل.
-    - ⛔ `/اغلق`* لإيقاف البوت والخروج من المحادثة الصوتية.""")
+    - ⛔ `/اغلق` لإيقاف البوت والخروج من المحادثة الصوتية.""")
 
-# saving the audio sended if its type is .mp3
-@client.on(events.NewMessage(func=lambda e: e.file and getattr(e.file, 'name', None) and (e.file.name.endswith('.mp3') or e.file.name.endswith('.ogg'))))
+# saving the audio sended
+@client.on(events.NewMessage(func=lambda e: e.file and (e.file.mime_type.startswith('audio') or e.file.name)))
 async def save_audio(event):
-
-    global bot_active
-
-    # check if the bot is ON
-    if not bot_active:
-        return
     
-    # check if the sende r is an admin
-    if not await is_admin(event):
-        await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
-        return 
+    chat_id = event.chat_id
+    
+    # check if the user bot is active
+    if chat_id not in active_groups: return
+    
+    check_if_admin(event)
     
     await event.reply("جار حفظ الملف ...")
-
-    chat_id = event.chat_id
-
-    # get the path to download the audio in
-    filename = event.file.name
+    
+    if event.file.name:
+        filename = event.file.name # get filename for external audio file
+    else:
+        filename = f"voice_{uuid.uuid4().hex}.ogg" # create a filename for Telegram vocals
+    
     file_path = os.path.join(SAVE_FOLDER, filename)
 
-    # save the audio file
+    # Save the audio file
     await event.download_media(file_path)
     uploaded_files[chat_id] = filename  
 
     await event.reply(f"✅ تم حفظ الملف بنجاح: `{filename}`")
 
+
 # join the chat voice and play the audio file
 @client.on(events.NewMessage(pattern="/شغل"))
 async def play_voice_chat(event):
     
-    global bot_active
-
-    # check if the bot is active
-    if not bot_active:
-        return
-    
-    # check if the sender is an admin
-    if not await is_admin(event):
-        await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
-        return
-
     chat_id = event.chat_id
+    
+    check_if_bot_active(chat_id, event)
+    check_if_admin(event)
 
     # check if the sender is replying on the wanted audio file
     if not event.reply_to_msg_id:
@@ -114,6 +115,9 @@ async def play_voice_chat(event):
     
     # searching for the audio file
     filename = uploaded_files.get(chat_id)
+    
+    # get audio file path
+    file_path = os.path.join(SAVE_FOLDER, filename)
 
     # check if the audio file is available
     if not filename:
@@ -125,11 +129,12 @@ async def play_voice_chat(event):
         await event.reply(f"🔊 جارٍ تشغيل: `{filename}`")
         try:
             await pytgcalls.start()
-            await pytgcalls.play(chat_id, os.path.join(SAVE_FOLDER, filename))
+            await pytgcalls.play(chat_id, file_path)
         except:
-            await pytgcalls.play(chat_id, os.path.join(SAVE_FOLDER, filename))
+            await pytgcalls.play(chat_id, file_path)
 
-        await event.reply("🎶 جارٍ تشغيل الملف الصوتي")
+        await event.reply("🎶 تم تشغيل الملف الصوتي")
+            
     except Exception as e:
         await event.reply("❌ خطأ أثناء التشغيل")
         print(f"Error: {e}")  
@@ -137,18 +142,11 @@ async def play_voice_chat(event):
 # pause the audio file
 @client.on(events.NewMessage(pattern="/توقف"))
 async def pause_voice_chat(event):
-    global bot_active
-
-    # check if the bot is active
-    if not bot_active:
-        return
-    
-    # check is the sender is an admin
-    if not await is_admin(event):
-        await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
-        return
     
     chat_id = event.chat_id
+     
+    check_if_admin(event)
+    check_if_bot_active(chat_id, event)
 
     # stoping the audio
     if event.is_group:
@@ -158,19 +156,11 @@ async def pause_voice_chat(event):
 # resume the audio file
 @client.on(events.NewMessage(pattern="/اكمل"))
 async def resume_voice_chat(event):
-
-    global bot_active
-
-    # check if the bot is active
-    if not bot_active:
-        return
     
-    # check if the sender is an admin
-    if not await is_admin(event):
-        await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
-        return
-
     chat_id = event.chat_id
+    
+    check_if_admin(event)
+    check_if_bot_active(chat_id, event)
 
     # resuming the audio
     if event.is_group:
@@ -180,21 +170,17 @@ async def resume_voice_chat(event):
 # stop the bot
 @client.on(events.NewMessage(pattern="/اغلق"))
 async def stop_bot(event):
-
-    global bot_active
-
-    # check if the sender is an admin
-    if not await is_admin(event):
-        await event.reply("🚫 فقط المشرفين يمكنهم استخدام هذا الأمر!")
-        return
-
-    bot_active = False
-
+    
     chat_id = event.chat_id
+
+    check_if_admin(event)
+    check_if_bot_active(chat_id, event)
     
     # stopping the bot and leaving the chat voice
-    await pytgcalls.leave_call(chat_id)
-    await event.reply("⛔ البوت متوقف الآن!")
+    if chat_id in active_groups:
+        active_groups.remove(chat_id)
+        await pytgcalls.leave_call(chat_id)
+        await event.reply("⛔ البوت متوقف الآن!")
 
 
 async def main():
